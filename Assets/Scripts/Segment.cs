3ef9e235ebaad4d3d904e1aa870197ff96368321
BryanMakers <BryanMakers>
2024-01-23 20:15:09 -0500

    init

---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class Segment : MonoBehaviour
+{
+    public Segment parent;
+    public List<Segment> children;
+    private Rigidbody rb => GetComponent<Rigidbody>();
+
+    void Start()
+    {
+        
+    }
+
+    public List<Segment> GetAllSegments()
+    {
+        Stack<Segment> segmentStack = new();
+        segmentStack.Push(this);
+        List<Segment> segments = new();
+        while (segmentStack.Count > 0)
+        {
+            Segment segment = segmentStack.Pop();
+            segments.Add(segment);
+            foreach(Segment childSegment in segment.children)
+            {
+                segmentStack.Push(childSegment);
+            }
+        }
+        return segments;
+    }
+
+    public void Fling()
+    {
+        // Fling for 3 seconds then destroy
+        parent.children.Remove(this);
+        foreach(Segment segment in GetAllSegments())
+        {
+            parent.children.Remove(segment);
+        }
+
+        rb.isKinematic = false;
+        rb.velocity = new Vector3(0,8,0);
+        Player.instance.Hurt();
+
+        StartCoroutine(SegmentDying());
+    }
+
+    public void HighlightAll()
+    {
+        foreach(Segment segment in GetAllSegments())
+        {
+            segment.Highlight();
+        }
+    }
+
+    public void UnhighlightAll()
+    {
+        foreach(Segment segment in GetAllSegments())
+        {
+            segment.Unhighlight();
+        }
+    }
+
+    public void Highlight()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void Unhighlight()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    IEnumerator SegmentDying()
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        Destroy(gameObject);
+    }
+}
